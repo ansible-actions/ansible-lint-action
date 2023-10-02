ec6e3a397e58af512e177202275967c3b7ab35ac
L3D <l3d@c3woc.de>
2023-10-02 23:37:00 +0200

    initialize Action

---
+++ ansible_docker.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Action to run ansible linter
+"""
+import subprocess
+import os
+import sys
+
+class EnvironmentManager:
+    """
+    Parsing Enviroment Variables
+    """
+    def __init__(self, env_var_name):
+        self.env_var_name = env_var_name
+        self.env_var_value = os.getenv(env_var_name)
+
+    def check_environment_variable(self):
+        """
+        Check if Variable is defined.
+        exit if undefined
+        """
+        if self.env_var_value is not None:
+            print(f"The value of {self.env_var_name} is: {self.env_var_value}")
+            return f"{self.env_var_value}"
+        print(f"The environment variable {self.env_var_name} is not set.\nFAILED")
+        sys.exit(1)
+
+class AnsibleCommandExecution:
+    """
+    running ansible commands
+    """
+    def run_command(self, command):
+        """
+        Running command as subprocess.
+        Printing error on fail and exit
+        """
+        try:
+            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+            return result.stdout
+        except subprocess.CalledProcessError as error:
+            print(f"Error running Ansible command: {error}\n\n{error.stdout}\n{error.stderr}")
+            sys.exit(1)
+
+if __name__ == "__main__":
+    ENV_TARGET_NAME = "TARGET"
+    env_target = EnvironmentManager(ENV_TARGET_NAME)
+    target = env_target.check_environment_variable()
+    print(target)
+
+
+    ansible_command = ["ansible-lint", f"{target}"]
+    ansible_version_checker = AnsibleCommandExecution()
+    version_info = ansible_version_checker.run_command(ansible_command)
