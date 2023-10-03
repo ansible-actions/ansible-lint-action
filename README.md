ca85f7ba11aaf29b817b2e061dfed42dcd1092d0
L3D <l3d@c3woc.de>
2023-10-03 02:56:59 +0200

    final adjustments for first version of this action

--- README.md
+++ README.md
@@ -4,3 +4,22 @@
 Linting Ansible roles...
 
 Work in Progress...
+
+```yml
+inputs:
+  target:
+    description: |
+      Target for ansible linter
+      For example './', 'roles/my_role/' or 'site.yml'
+    required: true
+  required_collections:
+    description: |
+      You can define a required ansible collection here.
+      They will be installed using ansible-galaxy collection install <YourInput>.
+    required: false
+  required_roles:
+    description: |
+      You can define a required ansible role here.
+      They will be installed using ansible-galaxy role install <YourInput>.
+    required: false
+```
--- ansible_docker.py
+++ ansible_docker.py
@@ -15,17 +15,28 @@ class EnvironmentManager:
         self.env_var_name = env_var_name
         self.env_var_value = os.getenv(env_var_name)
 
-    def check_environment_variable(self):
+    def check_required_environment_variable(self):
         """
-        Check if Variable is defined.
+        Check if required Variable is defined.
         exit if undefined
         """
         if self.env_var_value is not None:
             print(f"The value of {self.env_var_name} is: {self.env_var_value}")
             return f"{self.env_var_value}"
-        print(f"The environment variable {self.env_var_name} is not set.\nFAILED")
+        print(f"The variable {self.env_var_name} is not set but needs to be defined.\nFAILED")
         sys.exit(1)
 
+    def check_optional_environment_variable(self):
+        """
+        Check if optional Variable is defined.
+        exit if undefined
+        """
+        if self.env_var_value is not None:
+            print(f"The value of {self.env_var_name} is: {self.env_var_value}")
+            return f"{self.env_var_value}"
+        return False
+
+# pylint: disable=R0903
 class AnsibleCommandExecution:
     """
     running ansible commands
@@ -36,19 +47,46 @@ class AnsibleCommandExecution:
         Printing error on fail and exit
         """
         try:
-            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+            result = subprocess.run(command, stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE, text=True, check=True)
             return result.stdout
         except subprocess.CalledProcessError as error:
             print(f"Error running Ansible command: {error}\n\n{error.stdout}\n{error.stderr}")
             sys.exit(1)
 
 if __name__ == "__main__":
+    # define known enviroment vars
     ENV_TARGET_NAME = "TARGET"
+    ENV_REQUIRED_COLLECTION_NAME = "REQCOLLECTIONS"
+    ENV_REQUIRED_ROLE_NAME = "REROLE"
+
+    # check for target variable
     env_target = EnvironmentManager(ENV_TARGET_NAME)
-    target = env_target.check_environment_variable()
-    print(target)
+    target = env_target.check_required_environment_variable()
 
+    # check for required collection variable
+    env_collection = EnvironmentManager(ENV_REQUIRED_COLLECTION_NAME)
+    reqired_collection = env_collection.check_optional_environment_variable()
 
-    ansible_command = ["ansible-lint", f"{target}"]
+    # check for required role variable
+    env_role = EnvironmentManager(ENV_REQUIRED_ROLE_NAME)
+    reqired_role = env_role.check_optional_environment_variable()
+
+    # run ansible commands
     ansible_version_checker = AnsibleCommandExecution()
+
+    # Optionally install required ansible collections
+    if bool(reqired_collection):
+        ansible_command = ["ansible-galaxy", "collection", "install", f"{reqired_collection}", "--upgrade"]
+        version_info = ansible_version_checker.run_command(ansible_command)
+        print(f"COLLECTION INSTALL SUCCESSFUL\n{version_info}")
+
+    # Optionally install required ansible roles
+    if  bool(reqired_role):
+        ansible_command = ["ansible-galaxy", "role", "install", f"{reqired_role}", "--upgrade"]
+        version_info = ansible_version_checker.run_command(ansible_command)
+        print(f"ROLE INSTALL SUCCESSFUL\n{version_info}")
+
+    # run ansible lint
+    ansible_command = ["ansible-lint", f"{target}"]
     version_info = ansible_version_checker.run_command(ansible_command)
